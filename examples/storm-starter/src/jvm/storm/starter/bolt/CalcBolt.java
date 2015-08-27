83b74c9cd814a6a52805782e63811e1129dee6c4
jun young byun <bjy557@naver.com>
2015-08-27 14:44:53 +0900

    add string

--- examples/storm-starter/src/jvm/storm/starter/bolt/CalcBolt.java
+++ examples/storm-starter/src/jvm/storm/starter/bolt/CalcBolt.java
@@ -10,8 +10,9 @@ public class CalcBolt extends BaseBasicBolt{
 	@Override
 	public void execute(Tuple tuple, BasicOutputCollector collector) {
 		// TODO Auto-generated method stub
-		System.out.println("my name is.............." + tuple + "end...................." 
-		+ collector + "realend.............");
+		String word;
+		word = tuple.getString(0);
+		System.out.println("my name is..............  " + word + "  end....................");
 	}
 
 	@Override
