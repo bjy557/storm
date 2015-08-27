f886b7e78767c3c8745b54a1c9ed11978c9f8b5c
jun young byun <bjy557@naver.com>
2015-08-27 14:32:48 +0900

    change value arguments

--- examples/storm-starter/src/jvm/storm/starter/bolt/CalcBolt.java
+++ examples/storm-starter/src/jvm/storm/starter/bolt/CalcBolt.java
@@ -10,7 +10,7 @@ public class CalcBolt extends BaseBasicBolt{
 	@Override
 	public void execute(Tuple tuple, BasicOutputCollector collector) {
 		// TODO Auto-generated method stub
-		System.out.println("my name is.............." + tuple.getValue(0).toString() + "end....................");
+		System.out.println("my name is.............." + tuple.getValue(1) + "end....................");
 	}
 
 	@Override
